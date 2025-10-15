f5e5b564df662c82e71ec9a85c0b426b78cb75b4
Oluwatobiloba Zaccheaus <zacktobi2009@gmail.com>
2025-10-15 16:27:08 +0100

    Made some fixes.

--- week-3/practice_6/src/main.rs
+++ week-3/practice_6/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let interest:u8=8;
+    let interest:u8=8; //An unsigned 8 bit integer works weel here.
     println!("Interest is: {}.",interest );
 }
--- week-3/practice_7/src/main.rs
+++ week-3/practice_7/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let float_with_seperator:f32= 11_000.555_001;
+    let float_with_seperator:f32= 11_000.555_001; //Float with seperator
     println!("Float value: {}.", float_with_seperator);
 
-    let int_with_seperator:u32=50_000;
+    let int_with_seperator:u32=50_000; //Int with seperator.
     println!("Int value: {}.",int_with_seperator );
 
 }
