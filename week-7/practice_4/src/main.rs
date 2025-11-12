bc6ac215c8022bdf358ea0447ee2f184c4e6d657
Oluwatobiloba Zaccheaus <zacktobi2009@gmail.com>
2025-11-12 16:54:18 +0100

    Fixed practice 3, finished practice 4

---
+++ week-7/practice_4/src/main.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+fn add(a:i32,b:i32){
+    let sum=a+b;
+    println!("The sum is {}",sum);
+}
+
+fn main(){
+    let mut input1=String::new();
+    println!("Enter input for parameter A:");
+    io::stdin().read_line(&mut input1).expect("Failed to read line.");
+    let a:i32=input1.trim().parse().expect("Invalid input");
+
+    let mut input2=String::new();
+    println!("Enter input for parameter A:");
+    io::stdin().read_line(&mut input1).expect("Failed to read line.");
+    let b:i32=input2.trim().parse().expect("Invalid input");
+
+//call add function with arguments
+    add(a,b);
+
+}
