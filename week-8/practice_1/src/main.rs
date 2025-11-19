85a8ae56a3e738ca45e768847f750a8c3fad4d88
Oluwatobiloba Zaccheaus <zacktobi2009@gmail.com>
2025-11-19 16:18:20 +0100

    Created a vector in rust

---
+++ week-8/practice_1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let newVec:Vec<i64>=Vec::new();
+    println!("\nThe length of Vec::new is: {}",newVec.len());
+
+    let v=vec!["Grace","Effiong","Basil","Kareem", "Susan"];
+
+    //printing vector size
+    println!("\nThe length of vec macro is: {}",v.len());
+}
