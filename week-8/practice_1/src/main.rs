2ad8bbf3087a4bcfd5c627657890c6359660c02d
Oluwatobiloba Zaccheaus <zacktobi2009@gmail.com>
2025-11-19 16:19:27 +0100

    Finished practice 1

--- week-8/practice_1/src/main.rs
+++ week-8/practice_1/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let newVec:Vec<i64>=Vec::new();
-    println!("\nThe length of Vec::new is: {}",newVec.len());
+    let new_vec:Vec<i64>=Vec::new();
+    println!("\nThe length of Vec::new is: {}",new_vec.len());
 
     let v=vec!["Grace","Effiong","Basil","Kareem", "Susan"];
 
