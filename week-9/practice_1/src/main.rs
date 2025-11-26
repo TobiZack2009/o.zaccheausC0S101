feb900c3afa9ab0125876232e588268a49825cdb
Oluwatobiloba Zaccheaus <zacktobi2009@gmail.com>
2025-11-26 15:34:10 +0100

    Successfully appended to files/data.txt

---
+++ week-9/practice_1/src/main.rs
@@ -0,0 +1,14 @@
+use std::io::Write;
+use std::fs::*;
+fn main() {
+    let annouce="Week 9 - Rust File Input and Output";
+    let dep="Department of Computer Science";
+
+    let mut file=std::fs::File::create("../files/data.txt").expect("Creation failed!");
+    file.write_all("Welcome to Rust Programming\n".as_bytes()).expect("Creation Failed");
+    file.write_all(annouce.as_bytes()).expect("Write Failed!");
+    file.write_all(dep.as_bytes()).expect("Write failed!");
+    println!("Data Written to file!");
+
+
+}
